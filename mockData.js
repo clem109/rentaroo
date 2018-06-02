bd64edf48db49ed5b114c04b3f71b85f59fb11bc
Thomas Paradis <thomasparadis@126.com>
2018-06-02 15:52:19 +0800

    added property page, changed json file name

---
+++ mockData.js
@@ -0,0 +1,50 @@
+const ROOMS = ['single room + bathroom', 'two rooms + bathroom', 'three rooms + bathroom']
+
+module.exports = {
+    properties: [
+        {
+            photo: "",
+            location: "Beijing",
+            price: "3000",
+            id: "1",
+            date: "24-06-2018",
+            rooms: ROOMS[0]
+        }, {
+            photo: "",
+            location: "Shanghai",
+            price: "4000",
+            id: "2",
+            date: "23-06-2018",
+            rooms: ROOMS[2]
+        }, {
+            photo: "",
+            location: "Beijing",
+            price: "3000",
+            id: "3",
+            date: "24-06-2018",
+            rooms: ROOMS[1]
+        }, {
+            photo: "",
+            location: "Shanghai",
+            price: "4000",
+            id: "4",
+            date: "23-06-2018",
+            rooms: ROOMS[0]
+        }, {
+            photo: "",
+            location: "Beijing",
+            price: "3000",
+            id: "5",
+            date: "24-06-2018",
+            rooms: ROOMS[1]
+        }, {
+            photo: "",
+            location: "Shanghai",
+            price: "4000",
+            id: "6",
+            date: "23-06-2018",
+            rooms: ROOMS[2]
+        }
+    ]
+
+}
--- pages/property.js
+++ pages/property.js
@@ -1,17 +1,62 @@
+var mockData = require('../mockData.js')
+import Navbar from '../components/Navbar'
+
 export default class extends React.Component {
 
   // example of getting house data static async getInitialProps({req, res, query})
   // {   const id = query.id   // const response = await
   // fetch('https://api.tvmaze.com/search/shows?q=batman')   // const data = await
   // response.json() from the ID find the correct json   return {shows: data} }
+  static async getInitialProps({ req, res, query }) {
+    req.id = 2
+    const data = getPropertyById(mockData.properties, req.id)
+
+    return {
+      data: data
+    }
+  }
+
+
 
   render() {
 
     return (
       <div>
+        <section className="hero is-info is-large">
+          <Navbar />
+
+        </section >
         <p>Property page</p>
+        <div className="columns">
+          <div className="column">
+
+          </div>
+          <div className="column">
+            <div class="columns">
+              Second col
+            <div className="column">
+                {detail("Address", this.props.data.location)}
+              </div>
+              <div className="column">
+                {detail("Rooms", this.props.data.rooms)}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 
+}
+
+const detail = (name, val) => {
+  return (
+    <div>
+      <b>{name}</b> : {val}
+    </div>
+  )
+}
+
+const getPropertyById = (data, id) => {
+  return data.filter((el) => el.id == id)[0]
 }
