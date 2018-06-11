be7bb0a77ce2414b0e89835cea19cf0fd621a136
Clement Venard <cvenard@gmail.com>
2018-06-11 15:20:04 +0800

    fixed responsive bug

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,7 @@ export default class MyDocument extends Document {
         <body>
           <Main/>
           <NextScript/>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css"/>
